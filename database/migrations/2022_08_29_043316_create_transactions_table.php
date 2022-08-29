6c1ecbf792f62a71f17806d9e6e71541d121aa57
Enok Dilshan <73767944+Enok12@users.noreply.github.com>
2022-08-29 10:52:51 +0530

    first and completed commit

---
+++ database/migrations/2022_08_29_043316_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id('id');
+            $table->integer('user_id'); // User adding transaciton
+            $table->integer('qr_code_owner')->nullable();
+            $table->integer('qr_code_id');
+            $table->string('payment_method')->nullable(); //Paypal, Stripe, Paystack
+            $table->longText('message')->nullable()->default('initiated');
+            $table->float('amount',10,4);
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
