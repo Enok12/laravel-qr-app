c849f979df190abaabac2307271635330a20f0f8
Enok Dilshan <73767944+Enok12@users.noreply.github.com>
2022-09-01 20:22:37 +0530

    Design Update

--- resources/views/qrcodes/show.blade.php
+++ resources/views/qrcodes/show.blade.php
@@ -7,12 +7,15 @@
                 <div class="col-sm-6">
                     <h1>Qrcode Details</h1>
                 </div>
-                <div class="col-sm-6">
-                    <a class="btn btn-default float-right"
-                       href="{{ route('qrcodes.index') }}">
-                        Back
-                    </a>
-                </div>
+
+                @if($qrcode->user_id == Auth::user()->id || Auth::user()->role_id < 3)
+                    <div class="col-sm-6">
+                        <a class="btn btn-primary float-right"
+                        href="{{ route('qrcodes.edit', [$qrcode->id]) }}">
+                            Edit 
+                        </a>
+                    </div>
+                @endif
             </div>
         </div>
     </section>
