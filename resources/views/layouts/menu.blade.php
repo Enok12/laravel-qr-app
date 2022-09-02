9c0df8ffc4c07d2f7ed9df5a0894275c9b313b80
Enok Dilshan <73767944+Enok12@users.noreply.github.com>
2022-09-02 11:23:10 +0530

    Menu Rearrangement and Role Control

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,19 +1,4 @@
-<li class="nav-item">
-    <a href="{{ route('qrcodes.index') }}"
-       class="nav-link {{ Request::is('qrcodes*') ? 'active' : '' }}">
-        <p>Qrcodes</p>
-    </a>
-</li>
-
-
-<li class="nav-item">
-    <a href="{{ route('roles.index') }}"
-       class="nav-link {{ Request::is('roles*') ? 'active' : '' }}">
-        <p>Roles</p>
-    </a>
-</li>
-
-
+{{-- All User should have Access to this link --}}
 <li class="nav-item">
     <a href="{{ route('transactions.index') }}"
        class="nav-link {{ Request::is('transactions*') ? 'active' : '' }}">
@@ -21,12 +6,34 @@
     </a>
 </li>
 
+{{-- Web Masters --}}
+@if(Auth::user()->role_id < 4)
+    <li class="nav-item">
+        <a href="{{ route('qrcodes.index') }}"
+        class="nav-link {{ Request::is('qrcodes*') ? 'active' : '' }}">
+            <p>Qrcodes</p>
+        </a>
+    </li>
+@endif
 
-<li class="nav-item">
-    <a href="{{ route('users.index') }}"
-       class="nav-link {{ Request::is('users*') ? 'active' : '' }}">
-        <p>Users</p>
-    </a>
-</li>
+{{-- Moderators --}}
+@if(Auth::user()->role_id < 3)
+    <li class="nav-item">
+        <a href="{{ route('roles.index') }}"
+        class="nav-link {{ Request::is('roles*') ? 'active' : '' }}">
+            <p>Roles</p>
+        </a>
+    </li>
+@endif
+
+{{-- Admin --}}
+@if(Auth::user()->role_id == 1)
+    <li class="nav-item">
+        <a href="{{ route('users.index') }}"
+        class="nav-link {{ Request::is('users*') ? 'active' : '' }}">
+            <p>Users</p>
+        </a>
+    </li>
+@endif
 
 
