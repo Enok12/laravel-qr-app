ccb049bf9e3f9f866d00f30e4f1a74cabf75a7c8
Enok Dilshan <73767944+Enok12@users.noreply.github.com>
2022-09-03 10:35:31 +0530

    Middleware Setup for roles
    To avoid access of URLS

--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,8 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::group(['middleware' => 'auth'],function(){
 
 Route::resource('qrcodes', App\Http\Controllers\QrcodeController::class);
-Route::resource('roles', App\Http\Controllers\RoleController::class);
 Route::resource('transactions', App\Http\Controllers\TransactionController::class);
 Route::resource('users', App\Http\Controllers\UserController::class);
+Route::resource('roles', App\Http\Controllers\RoleController::class)->middleware('checkadmin');
 
 });
